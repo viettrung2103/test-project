0dba77821b9b2d7d4b455c65f86042d1b71a77c4
Trung_Doan <44894608+viettrung2103@users.noreply.github.com>
2024-09-02 20:52:50 +0300

    this is task 1new from trung subbranch

--- trung.js
+++ trung.js
@@ -1,3 +1,1 @@
-console.log("This is from trung subbranch, sprint1 branch");
-console.log("this is new trung branch");
-console.log("This is new task from trung branch")
+console.log("This is task 1 from trung sub branch");
