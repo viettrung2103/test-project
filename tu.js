30bb6ee79ac7950cc4efef3a1ce49caaae54c883
Trung_Doan <44894608+viettrung2103@users.noreply.github.com>
2024-09-02 21:06:45 +0300

    this is task 1 new from tu subbranch

--- tu.js
+++ tu.js
@@ -1,3 +1,3 @@
-console.log("This is task 1 from tu subbranch");
-console.log("This is task 2 from tu subbranch");
+console.log("This is task 1new from tu subbranch");
+console.log("This is task 2new from tu subbranch");
 console.log("This is task 3 from tu subbranch");
